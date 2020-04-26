2283c4553888cfd99d5af1e1e5a11053d2f9d8c7
Adrian Vazquez <aavm0000@gmail.com>
2020-04-26 01:15:18 -0400

    feat: random vertex linkage implemented and tested

--- main.cpp
+++ main.cpp
@@ -6,7 +6,13 @@
  */
 
 #include <iostream>
+#include <time.h>
 using namespace std;
+double  prob(void){
+	double result = rand();
+	result= result/RAND_MAX;
+return result ;
+}
 class graph{
 public:
 	graph(int n = 0){
@@ -16,6 +22,11 @@ public:
 			matrix[i] = new bool[size];
 		cout<<"Class Constructed , empty matrix, size :"<<size<<endl;
 	};
+	void print(void){
+		cout<<"se puede"<<endl;
+	};
+	void MonteCarlo(double density, int initialCost, int Range);
+	void printt(void);
 
 
 	~graph(){
@@ -29,8 +40,32 @@ private:
 	bool **matrix;
 	int size;
 };
+void graph::MonteCarlo(double density, int initialCost, int Range){
+	for(int i=0; i< graph::size;++i)
+		for(int j = i;j<graph::size;++j)
+			if (i==j)
+				graph::matrix[i][j]=graph::matrix[j][i]=false;
+			else
+				graph::matrix[i][j]=graph::matrix[j][i]=(prob()<density);
+	return ;
+}
+
+void graph::printt( void ){
+	for(int i=0; i< graph::size;++i){
+		cout<<endl;
+		for(int j = 0;j<graph::size;++j)
+			cout<<graph::matrix[i][j];
+	}
+	cout<<endl;
+}
+
 int main() {
-	graph g(4);
+	srand(time(0));
+	graph g(3);
+	g.MonteCarlo(0.1,2,3);
+	bool f = true;
+	cout<<f<<endl;
+	g.printt();
 	return 0;
 }
 
