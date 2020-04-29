00a851c4d13147fcdfd2e16a0649e3be42cf1905
Adrian Vazquez <aavm0000@gmail.com>
2020-04-29 10:58:42 -0400

    fix: Dijkstra working

--- main.cpp
+++ main.cpp
@@ -32,14 +32,16 @@ typedef struct {
 void insert_edge(AdjancencyList *g ,int x, int y, bool directed){
 	edgenode *p;
 	p = new edgenode;
-	p ->weight = 0;
+	p ->weight = 2;
 	p ->y =y;
 	p ->next = g->edges[x];
 	g->edges[x] = p;
 	g->degree[x] ++;
-	if (directed==false)
+	if (directed==false){
 		insert_edge(g,y,x,true);
+	}
 	else g->nedges++;
+	//cout<<"Edge inserted<< " <<x<<"<->"<<y<<" peso :"<< p ->weight<<endl;
 }
 class graph{
 public:
@@ -56,6 +58,7 @@ public:
 			List->degree[i] = 0;
 		for (i=0; i<n; i++)
 			List->edges[i] = NULL;
+		distance = new int[n];
 		cout<<"Class Constructed , empty matrix, size :"<<n<<endl;
 	};
 	void init(int n = 0, bool directed = false);
@@ -64,10 +67,12 @@ public:
 	//};
 	void MonteCarlo(double density, int initialCost, int Range);
 	//void printt(void);
+	int *dijkstra(int start);
 	void release(void){
 		delete [] List->degree;
 		delete [] List->edges;
 		delete List;
+		delete [] distance;
 	}
 
 	~graph(){
@@ -76,6 +81,7 @@ public:
 
 private:
 	AdjancencyList *List;
+	int *distance;
 };
 
 void graph::MonteCarlo(double density, int initialCost, int Range){
@@ -93,61 +99,80 @@ void graph::print_graph( void ){
 		cout<<i<<" :";
 		p=graph::List->edges[i];
 		while(p != NULL){
-			cout<<" "<<p->y;
+			cout<<"->"<<p->y;
 			p = p->next;
 		}
 	cout<<endl;
 	}
-}/*
-void dijkstra(graph g, int start, int distance[]){
+}
+int *graph::dijkstra(int start){
 	int i;
-	bool intree[g.size];
+	edgenode *p;
+	bool intree[graph::List->nvertices];
 	//int distance[g.size];
 	int v;
 	int w;
 	int weight;
 	int dist;
-	int parent[g.size];
-
-	for (i=1; i <= g.size;i++){
+	int *distance;
+	distance= new int[graph::List->nvertices];
+	for (i=0; i <graph::List->nvertices;i++){
 		intree[i] = false;
 		distance[i] = INT_MAX;
-		parent[i] = -1;
 	}
 	distance[start] =0;
 	v = start;
+	p = graph::List->edges[v];
 	while(!intree[v]){
 		intree[v] = true;
-		for(i =1;i <=g.size;i++){
-			weight = g.matrix[v][i];
-			while(weight!=0){
-				w = i;
+		cout<<v<<endl;
+		p = graph::List->edges[v];
+			while(p!=NULL){
+				w = p->y;
+				weight = p->weight;
 				if (distance[w] > (distance[v]+ weight)){
 					distance[w] = distance[v]+ weight;
-					parent[w] = v;
 				}
+				p = p ->next;
 			}
-		}
+
 		v =1;
 		dist = INT_MAX;
-		for(i =1; i <=g.size; i++){
+		for(i = 0; i < graph::List->nvertices ;i++)
 			if ((intree[i]==false)&&(dist>distance[i])){
 				dist = distance[i];
 				v = i;
+				break;
 			}
 
-		}
 	}
 
-}*/
+	graph::distance = distance;
+	for (int i = 0; i <4;i++){
+	if (distance[i] != INT_MAX)
+	cout<<distance[i]<<" ";
+	else cout<<-1<<" ";
+
+	}
+	cout<<endl;
+	delete [] distance;
+
+return graph::distance = distance;;
+}
 int main() {
 	srand(time(0));
-	graph g(1000,false);
+	graph g(4,false);
 	//g.init(1, false);
-	g.MonteCarlo(0.01,2,3);
-	bool f = true;
-	cout<<f<<endl;
+	g.MonteCarlo(1,2,3);
+	int *a;
+	a = new int[10];
+	//bool f = true;
+
 	g.print_graph();
+	a=g.dijkstra(0);
+
+
+	delete[] a;
 	return 0;
 }
 
